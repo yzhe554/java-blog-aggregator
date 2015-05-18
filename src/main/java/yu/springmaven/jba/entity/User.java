82c38cad6c24639bdaff8ab8ad86a8f536b08617
yzhe554 <lynnzheng1989@gmail.com>
2015-05-18 17:42:40 +1000

    Tutorial 8 Persistence

---
+++ src/main/java/yu/springmaven/jba/entity/User.java
@@ -0,0 +1,23 @@
+package yu.springmaven.jba.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
