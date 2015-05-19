3e31b159436ac083f968d098c8ea51483cc4a387
yzhe554 <lynnzheng1989@gmail.com>
2015-05-19 10:30:09 +1000

    Tutorial 11 MVC

---
+++ src/main/java/yu/springmaven/jba/service/UserService.java
@@ -0,0 +1,23 @@
+package yu.springmaven.jba.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import yu.springmaven.jba.entity.User;
+import yu.springmaven.jba.repository.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	
+}
