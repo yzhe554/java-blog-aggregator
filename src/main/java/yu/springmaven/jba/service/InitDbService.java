8b28b703a7120f923fdaab2800cddc74302213dd
yzhe554 <lynnzheng1989@gmail.com>
2015-05-20 16:15:18 +1000

    Tutorial 25 Add BCrypt

--- src/main/java/yu/springmaven/jba/service/InitDbService.java
+++ src/main/java/yu/springmaven/jba/service/InitDbService.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import yu.springmaven.jba.entity.Blog;
@@ -47,7 +48,8 @@ public class InitDbService {
 
 		User userAdmin = new User();
 		userAdmin.setName("admin");
-		userAdmin.setPassword("admin");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		userAdmin.setPassword(encoder.encode("admin"));
 		List<Role> roles = new ArrayList<Role>();
 		roles.add(roleAdmin);
 		roles.add(roleUser);
