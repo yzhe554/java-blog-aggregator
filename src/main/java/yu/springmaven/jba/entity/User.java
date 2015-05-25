5124abbe1623756adc24e918d14c6a045cb0c82a
yzhe554 <lynnzheng1989@gmail.com>
2015-05-26 01:16:41 +1000

    Tutorial 40 Custom Validation Annotation

--- src/main/java/yu/springmaven/jba/entity/User.java
+++ src/main/java/yu/springmaven/jba/entity/User.java
@@ -3,6 +3,7 @@ package yu.springmaven.jba.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,6 +14,8 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 
+import yu.springmaven.jba.annotation.UniqueUsername;
+
 @Entity
 public class User {
 
@@ -21,6 +24,8 @@ public class User {
 	private int id;
 	
 	@Size(min = 3, message = "Name must be at least 3 characters!")
+	@Column(unique=true)
+	@UniqueUsername(message="Such username already exists!")
 	private String name;
 	
 	@Size(min = 5, message = "Password must be at least 5 characters!")
