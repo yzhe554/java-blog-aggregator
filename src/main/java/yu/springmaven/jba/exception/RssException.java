52b5eb39580b52e72279f38d3dc8d0d099c6cb3e
yzhe554 <lynnzheng1989@gmail.com>
2015-05-26 11:03:53 +1000

    Tutorial 44 Junit

---
+++ src/main/java/yu/springmaven/jba/exception/RssException.java
@@ -0,0 +1,10 @@
+package yu.springmaven.jba.exception;
+
+public class RssException extends Exception {
+
+	public RssException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/test/java/yu/springmaven/jba/service/RssServiceTest.java
@@ -0,0 +1,33 @@
+package yu.springmaven.jba.service;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import yu.springmaven.jba.entity.Item;
+import yu.springmaven.jba.exception.RssException;
+
+public class RssServiceTest {
+
+	private RssService rssService;
+
+	@Before
+	public void setUp() throws Exception {
+		rssService = new RssService();
+	}
+
+	@Test
+	public void testGetItemsFile() throws RssException {
+		List<Item> items = rssService.getItems(new File("test-rss/javavids.xml"));
+		assertEquals(10, items.size());
+		Item firstItem = items.get(0);
+		assertEquals("How to solve Source not found error during debug in Eclipse", firstItem.getTitle());
+//		assertEquals("22 06 2014 20:35:49", new SimpleDateFormat("dd MM yyyy HH:mm:ss").format(firstItem.getPublishedDate()));
+	}
+
+}
