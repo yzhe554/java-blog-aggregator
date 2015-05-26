3074058c94fb064de24d84f1ffa9bd403e84558d
yzhe554 <lynnzheng1989@gmail.com>
2015-05-26 11:43:06 +1000

    Tutorial 47 Scheduling

--- src/main/java/yu/springmaven/jba/service/BlogService.java
+++ src/main/java/yu/springmaven/jba/service/BlogService.java
@@ -3,6 +3,7 @@ package yu.springmaven.jba.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -43,6 +44,14 @@ public class BlogService {
 			}
 		} catch (RssException e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Scheduled(fixedDelay=3600000) // 1 hour
+	public void reloadBlogs() {
+		List<Blog> blogs = blogRepository.findAll();
+		for (Blog blog : blogs) {
+			saveItems(blog);
 		}
 	}
 
