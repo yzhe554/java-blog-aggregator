ef35e4b24e9e547993679e53f8cfd1b389e2b3a9
yzhe554 <lynnzheng1989@gmail.com>
2015-05-26 15:22:01 +1000

    Redirect Attributes

--- src/main/java/yu/springmaven/jba/controller/RegisterController.java
+++ src/main/java/yu/springmaven/jba/controller/RegisterController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import yu.springmaven.jba.entity.User;
 import yu.springmaven.jba.service.UserService;
@@ -33,12 +34,13 @@ public class RegisterController {
 	}
 
 	@RequestMapping( method = RequestMethod.POST)
-	public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result) {
+	public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result, RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
 			return "user-register";
 		}
 		userService.save(user);
-		return "redirect:/register.html?success=true";
+		redirectAttributes.addFlashAttribute("success", true);
+		return "redirect:/register.html";
 	}
 	
 	@RequestMapping("/available")
